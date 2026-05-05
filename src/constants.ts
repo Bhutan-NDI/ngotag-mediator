f8dbc228bfaf23f0c08016106b879ba9a4eb3532
kinxa0 <kinzangdorji@bhutanndi.bt>
2026-05-05 11:50:10 +0600

    feat: add wallet database configuration options in .env

--- src/constants.ts
+++ src/constants.ts
@@ -28,3 +28,8 @@ export const USE_PUSH_NOTIFICATIONS = process.env.USE_PUSH_NOTIFICATIONS === 'tr
 export const NOTIFICATION_WEBHOOK_URL = process.env.NOTIFICATION_WEBHOOK_URL || 'http://localhost:5000'
 
 export const MESSAGE_FORWARDING_STRATEGY = process.env.MESSAGE_FORWARDING_STRATEGY
+
+export const WALLET_DB_MAX_CONNECTIONS = Number(process.env.WALLET_DB_MAX_CONNECTIONS) || 10
+export const WALLET_DB_MIN_CONNECTIONS = Number(process.env.WALLET_DB_MIN_CONNECTIONS) || 0
+export const WALLET_DB_IDLE_TIMEOUT = Number(process.env.WALLET_DB_IDLE_TIMEOUT) || 0
+export const WALLET_DB_CONNECT_TIMEOUT = Number(process.env.WALLET_DB_CONNECT_TIMEOUT) || 10

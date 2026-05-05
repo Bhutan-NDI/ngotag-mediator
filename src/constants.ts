2356153c24ae2e60b77c3e31bc90baf676c2c1d3
kinxa0 <kinzangdorji@bhutanndi.bt>
2026-05-05 11:27:45 +0600

    feat: add message forwarding strategy configuration and update environment example

--- src/constants.ts
+++ src/constants.ts
@@ -26,3 +26,5 @@ export const IS_DEV = process.env.NODE_ENV === 'development'
 export const USE_PUSH_NOTIFICATIONS = process.env.USE_PUSH_NOTIFICATIONS === 'true'
 
 export const NOTIFICATION_WEBHOOK_URL = process.env.NOTIFICATION_WEBHOOK_URL || 'http://localhost:5000'
+
+export const MESSAGE_FORWARDING_STRATEGY = process.env.MESSAGE_FORWARDING_STRATEGY
